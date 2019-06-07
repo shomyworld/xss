c4419923f5bc7f9466328f91a75a1fa0f3336f34
shoodagiri <bios1280336@gn.iwasaki.ac.jp>
2019-06-07 15:19:56 +0900

    ログイン後のセッションをseleniumに渡す機能の追加

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # XSS自動化
 - 指定したURLのフォームに、xss_patternを打ち込む
-
+- ログイン機能
 ## チケット
+- requestsでログインして、seleninumにログイン後のsessionを渡す
 - formが複数ある場合、それぞれのformにxss_patternを打ち込めるようにする
 - xss auditorの検知機能の追加
+- 自動ページ遷移機能の追加
--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 def getSoup(r):
@@ -10,7 +11,8 @@ def getSoup(r):
 
 
 def login():
-    url = input("target url > ")
+    # url = input("target url > ")
+    url = input()
     s = requests.session()
     r = s.get(url)
     soup = getSoup(r)
@@ -18,11 +20,22 @@ def login():
     element = {}
     for i in input_all:
         if i.get('value') is None:
-            element[i.get('name')] = input(i.get('name')+' : ')
+            # element[i.get('name')] = input(i.get('name')+' : ')
+            element[i.get('name')] = input()
         else:
             element[i.get('name')] = i.get('value')
     r = s.post(url, data=element)
-    print(r.text)
+    # ログイン後のsessionをseleniumに渡す
+    driver = webdriver.Chrome()
+    driver.get(url)
+    for name, value in s.cookies.get_dict().items():
+        tmp = {}
+        tmp['name'] = name
+        tmp['value'] = value
+        tmp['domain'] = '.qiita.com'
+        driver.add_cookie(tmp)
+    driver.get(url)
+    driver.close()
 
 
 if __name__ == '__main__':
